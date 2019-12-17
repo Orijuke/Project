25a88b08f8207e6bb5ea2ca745b3bfd943c1f9ea
Zaitsev Georgiy <zaitsevgeorge.ru@gmail.com>
2019-12-17 20:42:42 +0500

    Add generate_level function

--- block_class.py
+++ block_class.py
@@ -1,5 +1,6 @@
 from os import path
 from random import randrange
+import random
 
 import pygame
 
@@ -9,18 +10,40 @@ def load_image(name):
     image = pygame.image.load(fullname).convert_alpha()
     return image
 
+
 cell_size = 50
-size = width, height = 600, 150
+size = width, height = 1200, 600
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 FPS = 30
+
+def generate_level():
+    y_last = height // cell_size - 3
+    generated_map = [[-1 for i in range(width // cell_size)] for j in range(height // cell_size)]
+    for x in range(width // cell_size):
+        y = random.choice([y_last - 1, y_last, y_last + 1])
+        print(y)
+        while height // cell_size < y + 1 or y + 1 < 1:
+            print(y)
+            y = random.choice([y_last - 1, y_last, y_last + 1])
+        generated_map[y - 1][x] = 1
+        for n_y in range(y, height // cell_size):
+            generated_map[n_y][x] = 0
+        y_last = y
+    return generated_map
+
+generate_level()
+
+
+
+
 
 class Block(pygame.sprite.Sprite):
 
     grass_image = load_image("grass.png")
     dirt_image = load_image("dirt.png")
 
-    def __init__(self, x, y, grassed=False):
+    def __init__(self, x, y, grassed):
         super().__init__()
         if grassed:
             self.image = Block.grass_image
@@ -34,14 +57,16 @@ class Block(pygame.sprite.Sprite):
         self.rect.x = x * cell_size
         self.rect.y = y * cell_size
 
+
 all_sprites = pygame.sprite.Group()
+level_map = generate_level()
 
 blocks = []
-for i in range(15):
-    block = Block(randrange(0, width // cell_size), randrange(0, height // cell_size))
-    while pygame.sprite.spritecollideany(block, all_sprites):
-        block.set_pos(randrange(0, width // cell_size), randrange(0, height // cell_size))
-    all_sprites.add(block)
+for y in range(0, height // cell_size):
+    for x in range(width // cell_size):
+        if level_map[y][x] != -1:
+            block = Block(x, y, level_map[y][x])
+            all_sprites.add(block)
 
 
 is_running = True
